6fbd61747a2a4c7941d81abdcb61a443bef395e5
Richie Mackay <richie.mackay@gmail.com>
2017-03-01 22:25:33 +0000

    first stab - tests passing

--- ecmascript/beer-song/babel-output/beer-song.js
+++ ecmascript/beer-song/babel-output/beer-song.js
@@ -1,26 +1,53 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
     value: true
 });
-exports["default"] = {
+exports['default'] = {
     verse: function verse(number) {
+        function capitalizeFirstLetter(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+
         var getBottles = function getBottles(number) {
             if (number === 0) {
-                return "no more bottles";
-            } else if (number === 1) {
-                return "1 bottle";
+                return 'no more bottles';
             }
-            return number + " bottles";
+            if (number === 1) {
+                return '1 bottle';
+            }
+            return number + ' bottles';
         };
 
         var getPronoun = function getPronoun(number) {
-            return number === 1 ? "it" : "one";
+            return number === 1 ? 'it' : 'one';
         };
 
-        return getBottles(number) + " of beer on the wall, " + getBottles(number) + " of beer.\nTake " + getPronoun(number) + " down and pass it around, " + getBottles(number - 1) + " of beer on the wall.\n";
+        var getFirstLine = function getFirstLine(number) {
+            return capitalizeFirstLetter(getBottles(number)) + ' of beer on the wall, ' + getBottles(number) + ' of beer.\n';
+        };
+
+        var getLastLine = function getLastLine(number) {
+            if (number > 0) {
+                return 'Take ' + getPronoun(number) + ' down and pass it around, ' + getBottles(number - 1) + ' of beer on the wall.\n';
+            } else {
+                return 'Go to the store and buy some more, ' + getBottles(99) + ' of beer on the wall.\n';
+            }
+        };
+
+        return getFirstLine(number) + getLastLine(number);
     },
 
-    sing: function sing(start, end) {}
+    sing: function sing(start, end) {
+        start = start || 99;
+        end = end || 0;
+
+        var song = '';
+        for (var i = start; i >= end; i--) {
+            song += this.verse(i);
+            song += i !== end ? '\n' : '';
+        }
+        return song;
+    }
 };
-module.exports = exports["default"];
+module.exports = exports['default'];
--- ecmascript/beer-song/beer-song.js
+++ ecmascript/beer-song/beer-song.js
@@ -1,10 +1,15 @@
 export default  {
     verse(number)
     {
+        function capitalizeFirstLetter(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+
         const getBottles = (number) => {
             if (number === 0) {
                 return `no more bottles`
-            } else if (number === 1) {
+            }
+            if (number === 1) {
                 return `1 bottle`
             }
             return `${number} bottles`
@@ -14,12 +19,29 @@ export default  {
             return number === 1 ? `it` : `one`;
         };
 
-        return `${getBottles(number)} of beer on the wall, ${getBottles(number)} of beer.
-Take ${getPronoun(number)} down and pass it around, ${getBottles(number - 1)} of beer on the wall.
-`
+        const getFirstLine = (number) => `${capitalizeFirstLetter(getBottles(number))} of beer on the wall, ${getBottles(number)} of beer.\n`;
+
+        const getLastLine = (number) => {
+            if (number > 0) {
+                return `Take ${getPronoun(number)} down and pass it around, ${getBottles(number - 1)} of beer on the wall.\n`
+            } else {
+                return `Go to the store and buy some more, ${getBottles(99)} of beer on the wall.\n`
+            }
+        };
+
+        return (getFirstLine(number) + getLastLine(number))
     },
 
-    sing(start, end){
+    sing(start, end)
+    {
+        start = start || 99;
+        end = end || 0;
 
+        let song = ``;
+        for (let i = start; i >= end; i--) {
+            song += this.verse(i);
+            song += i !== end ? '\n' : ``
+        }
+        return song;
     }
 }
