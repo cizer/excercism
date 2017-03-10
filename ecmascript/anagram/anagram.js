3c65b2cf9c717e7630061a3dd8411bca097f7d87
Richie Mackay <richie.mackay@gmail.com>
2017-03-10 23:41:21 +0000

    submit anagram

---
+++ ecmascript/anagram/anagram.js
@@ -0,0 +1,21 @@
+export default function(_anagram) {
+  return {
+    matches(...words) {
+      const wordList = typeof words[0] === "string" ? words : words[0];
+      const anagram = _anagram.toLowerCase();
+
+      return wordList.filter(word => {
+        let clone = anagram;
+
+        [...word.toLowerCase()].forEach(a => {
+          const excisePoint = clone.indexOf(a);
+          clone = clone.substr(0, excisePoint) + clone.substr(excisePoint + 1);
+        });
+
+        return clone.length === 0 &&
+          anagram.length === word.length &&
+          anagram !== word.toLowerCase();
+      });
+    }
+  };
+}
