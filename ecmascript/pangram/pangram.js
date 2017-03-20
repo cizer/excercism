4d6ac9e418a7e2f2d526a774c1c8cf0f7604fb97
Richie Mackay <richie.mackay@gmail.com>
2017-03-20 23:16:03 +0000

    editor config and reformatted pangram

--- ecmascript/pangram/pangram.js
+++ ecmascript/pangram/pangram.js
@@ -1,15 +1,14 @@
 export default function (sentence) {
 
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-    return {
-        isPangram()
-        {
-            return alphabet
-                    .filter(a => !sentence
-                        .toLowerCase()
-                        .includes(a)).length === 0;
-        }
+  return {
+    isPangram()
+    {
+      return alphabet
+          .filter(a => !sentence
+            .toLowerCase()
+            .includes(a)).length === 0;
     }
-
+  }
 }
